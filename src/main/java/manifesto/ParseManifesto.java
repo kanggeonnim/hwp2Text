86b6c28bfc73dc7e6b24506d2f1fc326da3f8497
kanggeonnim <gun4329@gmail.com>
2024-03-08 17:59:29 +0900

    add string Row

--- src/main/java/manifesto/ParseManifesto.java
+++ src/main/java/manifesto/ParseManifesto.java
@@ -2,6 +2,9 @@ package manifesto;
 
 import hwp.Document;
 import hwp.HwpToText2;
+import hwp.Table;
+import hwp.TableRow;
+import kr.dogfoot.hwplib.object.bodytext.control.table.Row;
 
 import java.io.File;
 
@@ -24,10 +27,25 @@ public class ParseManifesto {
         Document document = h2.hwpToText(DATA_DIRECTORY + "057.220831_더불어민주당 부산북강서갑_전재수 의원실_21대 국회의원 공약이행 및 의정활동 평가.hwp");
         String name = document.getTables().get(1).getRows().get(0).getCells().get(1);
         String standingCommittee = document.getTables().get(1).getRows().get(0).getCells().get(3);
+        System.out.println("테이블 수: " + document.getTables().size());
         System.out.println("의원명: " + name);
         System.out.println("상임위원회: " + standingCommittee);
         System.out.println("=================================================================");
-        System.out.println(document);
+
+        int i = 1;
+        for (Table t : document.getTables()) {
+            System.out.println("===========");
+            System.out.println("Table " + i);
+            int j = 1;
+            for (TableRow tr : t.getRows()) {
+                System.out.println("-----------");
+                System.out.println("Row " + j);
+                System.out.println(tr);
+                j++;
+            }
+            System.out.println(t);
+            i++;
+        }
     }
 
 }
